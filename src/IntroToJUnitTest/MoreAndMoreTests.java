f9cff002204c9727db2b7eacd570535932f7cf3d
Ryguy-1 <league@WorthyMini-5.attlocal.net>
2017-10-08 12:56:07 -0700

    Finished on working on the square check.

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -8,42 +8,73 @@ import org.junit.Test;
  * Uncomment each test and then write the method to make it pass.
  * **/
 
-public class MoreAndMoreTests {
+public class MoreAndMoreTests{
+	
+	@Test
+	  public void MultTest() {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+		public String multiply(int x, int y) {
+		
+		int value = x*y;
+		
+		String value2 = ""+value;
+		
+		return x+" x "+y+" = "+value2;
+		
+	}
+		
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+	boolean isPrimeBool = false;
+	public boolean isPrime(int x) {
+		
+		for (int i = 2; i < x-1; i++) {
+			
+			if(x%i==0) {
+				return false;
+			}
+	}
+		return true;
+		
+	}
+	
+
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));	    
+	    assertFalse(isSquare(143));
+
+	  }
+
+	public boolean isSquare(int x) {
+		
+		if(Math.sqrt(x)%2 == 0) {
+		return true;
+		
+	}else {
+		return false;
+	}
+	
 //	@Test
 //	  public void CubeTest() {
 //
---
+++ src/IntroToJUnitTest/UnitTest.java
@@ -0,0 +1,15 @@
+package IntroToJUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+
+}
